6f3e725cca20b26e73c89d19811e9147af1e9e56
射命丸文 <shameimaru.ayaaya@gmail.com>
2025-08-18 17:38:20 +0900

    Update hook-cv2.py

--- hooks/hook-cv2.py
+++ hooks/hook-cv2.py
@@ -1,18 +1,6 @@
 # hooks/hook-cv2.py
-from PyInstaller.utils.hooks import collect_all, collect_data_files, get_module_file_attribute
-import os
-import glob
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
-# 收集所有 cv2 模块及其依赖
-datas, binaries, hiddenimports = collect_all('cv2')
-
-# 显式添加 config.py 和 config-*.py
-cv2_dir = os.path.dirname(get_module_file_attribute('cv2'))
-config_files = glob.glob(os.path.join(cv2_dir, "config*.py"))
-for f in config_files:
-    datas.append((f, 'cv2'))
-
-# 确保 cv2 的 .so 或 .pyd 文件也被打包
-for ext in ('*.pyd', '*.so'):
-    for f in glob.glob(os.path.join(cv2_dir, ext)):
-        binaries.append((f, 'cv2'))
+# 只收集子模块和数据文件
+hiddenimports = collect_submodules('cv2')
+datas = collect_data_files('cv2')
